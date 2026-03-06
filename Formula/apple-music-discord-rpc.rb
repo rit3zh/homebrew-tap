d8eebbed1a528161ece8d4874428f96c055be4fe
Ritesh. <119659853+rit3zh@users.noreply.github.com>
2026-03-06 11:34:07 -0500

    chore: bump apple-music-discord-rpc to v1.1.0

--- Formula/apple-music-discord-rpc.rb
+++ Formula/apple-music-discord-rpc.rb
@@ -1,11 +1,12 @@
 class AppleMusicDiscordRpc < Formula
   desc "Apple Music rich presence for Discord"
   homepage "https://github.com/rit3zh/apple-music-discord-rpc"
-  url "https://github.com/rit3zh/apple-music-discord-rpc/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "002f3a1bfb201bc2059277f1eab5e7597aae78f562e2b44ed6a8670db2acf9f9"
+  url "https://github.com/rit3zh/apple-music-discord-rpc/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "e8e4eb1b601561a803a82574737d31db185899aea54b564f54f1bcf141e22544"
   license "MIT"
 
   depends_on "oven-sh/bun/bun"
+  depends_on "ffmpeg"
 
   service do
     run [opt_bin/"apple-music-discord-rpc"]
@@ -28,6 +29,13 @@ class AppleMusicDiscordRpc < Formula
       export TMPDIR=$(getconf DARWIN_USER_TEMP_DIR)
       export PATH="#{HOMEBREW_PREFIX}/bin:#{HOMEBREW_PREFIX}/sbin:/usr/local/bin:/usr/bin:/bin:$PATH"
       exec "#{Formula["oven-sh/bun/bun"].opt_bin}/bun" "#{libexec}/index.ts" "$@"
+    SH
+
+    (bin/"apple-music-rpc").write <<~SH
+      #!/bin/bash
+      export TMPDIR=$(getconf DARWIN_USER_TEMP_DIR)
+      export PATH="#{HOMEBREW_PREFIX}/bin:#{HOMEBREW_PREFIX}/sbin:/usr/local/bin:/usr/bin:/bin:$PATH"
+      exec "#{Formula["oven-sh/bun/bun"].opt_bin}/bun" "#{libexec}/cli.ts" "$@"
     SH
   end
 
